0f9b5c30540e1cf7a5fb33d049b265f22fc1378d
jongwhan lee <leejw51@gmail.com>
2019-05-01 22:50:13 +0900

    update array

---
+++ updatearray.hs
@@ -0,0 +1,8 @@
+import Data.Array
+main = test2 2 10
+test2 s e = do
+    let a = array (s,e) [(i, 2000) | i <- [s..e]]
+    let a' = a // [(s,100)]
+    putStrLn $ "original: " ++ show a
+    putStrLn $ "new: " ++ show a'
+    
