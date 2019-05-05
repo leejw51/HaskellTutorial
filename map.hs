619999ccba4618e930169e485834ebdcb91c96f7
jongwhan lee <leejw51@gmail.com>
2019-05-05 20:28:28 +0900

    add map

---
+++ map.hs
@@ -0,0 +1,9 @@
+myMap  func [] = []
+myMap  func (x:xs) = func x:(myMap func  xs)
+
+main = do
+  print "ok"
+  let a= map (\x -> x + 2) [1..5] 
+  print a
+  let b= myMap (\x -> x + 2) [1..5] 
+  print b
