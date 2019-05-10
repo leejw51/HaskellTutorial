bcd615f8321e4e311e8cb51d3b81136f4c718b49
jongwhan lee <leejw51@gmail.com>
2019-05-10 17:39:08 +0900

    loop

--- merkletrie/src/My.hs
+++ merkletrie/src/My.hs
@@ -3,7 +3,9 @@ module My where
 import Data.Digest.Pure.SHA
 import Data.ByteString.Lazy.UTF8 
 
+-- grouped expression should be aligned
 -- ok
-myFunc = do
- putStrLn "pleae enter string =" >>
-  getLine >>= \a -> print $ sha256 $ fromString a
+myFunc = do putStrLn "pleae enter string2 =" 
+            a <- getLine  
+            print $ sha256 $ fromString a
+            myFunc
