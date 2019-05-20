d982bf3e039d4042892ff0408634df0045563301
leejw51 <leejw51@gmail.com>
2019-05-20 16:52:38 +0900

    bubble sort 3

---
+++ bubblesort3.hs
@@ -0,0 +1,14 @@
+mysort[] = []
+mysort[x] = [x]
+
+mysort a = do
+ let (x:y:xs) = a
+ let    ret =  mysort(init bubble) ++ [last bubble]
+         where 
+        bubble= if (x<=y) then [x] ++ mysort(y: xs) else [y] ++ mysort(x: xs)  
+ ret
+
+main = do
+ let b= mysort [10, 5, 20,1]
+ print b
+ 
