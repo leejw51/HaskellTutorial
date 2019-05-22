6622b910babaf7075f4e429bffef2cd08509f33c
jongwhan lee <leejw51@gmail.com>
2019-05-22 16:43:29 +0900

    if

---
+++ if.hs
@@ -0,0 +1,14 @@
+mydisplay 0 = 0
+mydisplay a = do
+    let b =  if rem a 2 == 0 then a+ mydisplay (a-1) else mydisplay(a-1)
+    b
+
+mydisplay2 0 = 0
+mydisplay2 a = do
+    let b =  if rem a 2 == 1 then a+ mydisplay2 (a-1) else mydisplay2(a-1)
+    b    
+
+main = do
+    print $ mydisplay 10
+    print $ mydisplay2 10
+
