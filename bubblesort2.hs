bab44773cd1e2551cfc31a98dbcf1900aeb1521c
jongwhan lee <leejw51@gmail.com>
2019-05-30 12:47:54 +0900

    matrix2

--- bubblesort2.hs
+++ bubblesort2.hs
@@ -6,7 +6,7 @@ mysort x = do
  let bubbled = if (a < b)
                 then [a] ++ mysort(b:c) 
                 else [b] ++ mysort(a:c)
- let ret = mysort(init bubbled) ++ [last bubbled]
+ --let ret = mysort(init bubbled) ++ [last bubbled]
  ret
 
 main = do 
