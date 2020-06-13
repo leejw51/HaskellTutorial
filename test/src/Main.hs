4103cc034d4e424dff4db3228311e8bb771a78e1
jongwhan lee <jonghwan@crypto.com>
2020-06-14 00:59:47 +0800

    test

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,8 +1,12 @@
 module Main where
 
+import           Test
+
 main :: IO ()
 main = do
   putStrLn hello
+  print $ compute 10
+  print $ add_apple 20
 
 hello :: String
 hello = "Hello"
---
+++ test/src/Test.hs
@@ -0,0 +1,5 @@
+module Test where
+
+compute x = x * 2
+
+add_apple x = x + 100
