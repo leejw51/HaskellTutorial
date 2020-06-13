438a60cd7705a269fd831f146eeb6aed06185827
jongwhan lee <jonghwan@crypto.com>
2020-06-14 00:34:48 +0800

    OK

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,12 +1,4 @@
 module Main where
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  putStrLn "OK"
-  print $ sayMe 1
-
-sayMe :: (Integral a, Integral b) => a -> b
-sayMe 1 = 100
-sayMe 5 = 500
-sayMe x = 0
+main = putStrLn "OK"
