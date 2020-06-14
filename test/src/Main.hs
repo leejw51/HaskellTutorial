4c18f493683a25e08e45c53e06e2fc01a7399041
jongwhan lee <jonghwan@crypto.com>
2020-06-14 20:22:42 +0800

    print

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,16 +1,9 @@
 module Main where
 
-import           Control.Monad
-import           Data.Char     (toUpper)
-import           Test
+main = show_all 10
 
-main :: IO ()
-main = do
-  putStrLn "write=" >> fmap shout getLine >>= putStrLn >> putStrLn "  apple ]["
-
-shout = map toUpper
-
-hello :: Float -> Float
-hello a = do
-  let b = a + 10
-  b
+show_all 0 = return ()
+show_all n = do
+  putStrLn "----"
+  print $ n
+  show_all (n - 1)
--- test/src/Test.hs
+++ test/src/Test.hs
@@ -1,5 +1,22 @@
 module Test where
 
+import           Control.Monad
+import           Data.Char     (toUpper)
+
 compute x = x * 2
 
 add_apple x = x + 100
+
+main2 = do
+  putStrLn "write=" >> fmap shout getLine >>= putStrLn >> putStrLn "  apple ]["
+
+shout = map toUpper
+
+hello a = do
+  let b = a + 10
+  b
+
+show_all 0 = return ()
+show_all a = do
+  putStrLn a
+  show_all (a - 1)
