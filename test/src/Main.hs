1f2325840292b6076a8e8215c199bb762d4fe0d6
jongwhan lee <jonghwan@crypto.com>
2020-06-14 20:35:02 +0800

    LOOP

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,9 +1,16 @@
 module Main where
 
-main = do
-  show_all 10
-  return ()
+import           Control.Monad
 
+main :: IO ()
+main = do
+  show_all 5
+
+main2 = do
+  a <- getLine
+  putStrLn a
+
+show_all :: Int -> IO ()
 show_all 0 = return ()
 show_all n = do
   putStrLn "----"
