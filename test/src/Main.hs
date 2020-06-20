c7e79aeda5ce4a250440bb322f3f6ef2e885ec64
jongwhan lee <jonghwan@crypto.com>
2020-06-20 22:56:35 +0800

    main

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,18 +1,5 @@
 module Main where
 
-import           Control.Monad
-
-main :: IO ()
+main:: IO ()
 main = do
-  putStrLn "enter" >> getLine >>= putStrLn
-
-main2 = do
-  a <- getLine
-  putStrLn a
-
-show_all :: Int -> IO ()
-show_all 0 = return ()
-show_all n = do
-  putStrLn "----"
-  print $ n
-  show_all (n - 1)
+  putStrLn "OK"
