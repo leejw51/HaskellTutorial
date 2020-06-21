2cbcd6b656b2c9a9d6f62aa2ba0f67dcdfb7c436
jongwhan lee <jonghwan@crypto.com>
2020-06-21 12:37:48 +0800

    haskell

--- apple/Main.hs
+++ apple/Main.hs
@@ -4,14 +4,13 @@ import Numeric (showHex)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BS2
 import qualified Crypto.Hash.SHA256 as SHA256
-import qualified Crypto.Hash.SHA1 as SHA1
 main :: IO ()
 main = do
   let s = "sha1 this string"
   let bs = SHA256.hash $ BS2.pack s    
-  Prelude.putStrLn s
+  putStrLn s
   putStrLn $ concatMap (flip showHex "") $ BS.unpack bs
   print$  BS.unpack bs
   --putStrLn $ showHex 100,1,3] "--"
   print $ concatMap (\x -> [(x,x+2,x/2)]) [1,3,5] 
-  Prelude.putStrLn "OK"
+  putStrLn "OK"
