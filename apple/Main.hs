a3bb892c31030427edf5a6b137c73a2db80eba07
jongwhan lee <jonghwan@crypto.com>
2020-06-21 14:40:49 +0800

    type

--- apple/Main.hs
+++ apple/Main.hs
@@ -2,10 +2,19 @@ module Main where
 import Test (test_main)
 import Sha (sha_main)
 import Text.Format
+import System.IO
 main= do  
   --test_main
   --sha_main
-  a <- getLine 
-  let b= format "Data= {0}" [a]
-  putStrLn b
+  --a <- getLine 
+  --let b= format "Data= {0}" [a]
+  run_loop 10
+  --putStrLn b
+  putStrLn "OK"
 
+
+run_loop:: Integer -> IO ()
+run_loop 0 = return ()
+run_loop  x = do
+  putStrLn $ show x
+  run_loop (x-1)
