dd5af675d09bd06d5145eaa28b12417bb4be6c1a
jongwhan lee <jonghwan@crypto.com>
2020-06-21 14:00:25 +0800

    add format

--- apple/Main.hs
+++ apple/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 import Test (test_main)
 import Sha (sha_main)
-
+import Text.Format
 main= do  
-  test_main
-  sha_main
-  putStrLn "OK"
+  --test_main
+  --sha_main
+  a <- getLine 
+  let b= format "Data= {0}" [a]
+  putStrLn b
 
