6627fd20c626664dcf8b9ff86c434a7484237f07
jongwhan lee <jonghwan@crypto.com>
2020-06-29 00:37:58 +0800

    OK

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,5 +1,20 @@
 import           Test
 
 main = do
+  runb [1..10]
   putStrLn "OK"
-  print $ compute 10
+
+
+runa 0 = putStrLn ""
+runa a = do 
+  print $ a
+  runa (a-1)
+
+
+runb [] = do
+  putStrLn "done"
+runb a = do
+  print $ head a
+  let b= drop 1 a
+  runb b
+  
