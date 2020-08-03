e9cdcfcc15bd201f96b6e574e7f72da75355db84
jonghwan <jonghwan@crypto.com>
2020-08-04 00:49:45 +0800

    print

--- monad/src/Lib.hs
+++ monad/src/Lib.hs
@@ -7,6 +7,18 @@ module Lib
 apple = do
   return "HELLO"
 
+
+buy2 (Just a) = return (a + 1000) 
+buy (Just a)= print a
+buy3 a = Just (a*1000)
 pear = putStrLn "pear"
+
+
+myprint  a = print a
 someFunc :: IO ()
-someFunc =  apple >>= putStrLn >> pear
+someFunc= do
+  let a1=Just 200 >>= buy3 >>= (\x -> Just (x+100)) >>= ( \x -> Just x)
+  buy a1
+  print "OK"
+
+someFunc2 =  apple >>= putStrLn >> pear
