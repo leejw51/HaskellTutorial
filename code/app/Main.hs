4e5b8ac18de0e2f2e03433aeabd84b2fe4c99774
jongwhan lee <leejw51@gmail.com>
2020-08-15 23:23:04 +0800

    add thread

--- code/app/Main.hs
+++ code/app/Main.hs
@@ -1,14 +1,5 @@
 module Main where
 
-import Control.Concurrent (forkIO)
-import qualified Data.ByteString.Lazy as L
-import Data.Digest.Pure.MD5 (md5)
-import System.Environment (getArgs)
+import Thread
 
-main
-  = do [fileA, fileB] <- getArgs
-       forkIO $ hashAndPrint fileA
-       hashAndPrint fileB
-
-hashAndPrint f
-  = L.readFile f >>= return . md5 >>= \ h -> putStrLn (f ++ ": " ++ show h)
+main = thread_main
---
+++ code/app/Thread.hs
@@ -0,0 +1,14 @@
+module Thread where
+
+import Control.Concurrent (forkIO)
+import qualified Data.ByteString.Lazy as L
+import Data.Digest.Pure.MD5 (md5)
+import System.Environment (getArgs)
+
+thread_main
+  = do [fileA, fileB] <- getArgs
+       forkIO $ hashAndPrint fileA
+       hashAndPrint fileB
+
+hashAndPrint f
+  = L.readFile f >>= return . md5 >>= \ h -> putStrLn (f ++ ": " ++ show h)
