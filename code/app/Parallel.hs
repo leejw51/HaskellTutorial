2203e545ef779c8b58221637eed518df46aa6c3d
jongwhan lee <leejw51@gmail.com>
2020-08-16 17:02:24 +0800

    tidy up

--- code/app/Parallel.hs
+++ code/app/Parallel.hs
@@ -7,11 +7,13 @@ import Data.Time.Clock
 import System.Environment
 import Text.Printf
 
+fib2 :: (Eq a1, Num a2, Num a1) => a1 -> [a2]
 fib2 1 = 1 : []
 fib2 2 = 1 : 1 : []
 fib2 n = sum (take 2 f) : f
   where f = fib2 (n - 1)
 
+fib3 :: (Eq a1, Num a, Num a1) => a1 -> a
 fib3 n = fib2 n !! 0
 
 -- slow fibonacci
@@ -20,10 +22,13 @@ fib 0 = 1
 fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
+printNumber :: Show a => [a] -> IO ()
 printNumber [] = return ()
 printNumber (x : xs)
   = do putStrLn $ show x
        printNumber xs
+
+parallel_main :: IO ()
 parallel_main
   = do let test = test_fast
        t0 <- getCurrentTime
@@ -41,6 +46,7 @@ test_fast
        z1 <- rpar (fib3 4000)
        return [x, y, z, z1]
 
+printTimeSince :: UTCTime -> IO ()
 printTimeSince t0
   = do t1 <- getCurrentTime
        printf "time: %.9fs seconds\n" (realToFrac (diffUTCTime t1 t0) :: Double)
