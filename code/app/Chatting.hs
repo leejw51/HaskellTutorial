b713298e93011427ce0b5899ba578204a7d4c420
jongwhan <jonghwan@crypto.com>
2020-08-27 01:18:45 +0800

    formatting
    haskell with c

--- code/app/Chatting.hs
+++ code/app/Chatting.hs
@@ -1,21 +1,28 @@
 module Chatting where
 import Network.Socket
 
-chatting_main = do
-    sock <- socket AF_INET Stream 0    -- create socket
-    setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
-    bind sock (SockAddrInet 4242 iNADDR_ANY)   -- listen on TCP port 4242.
-    listen sock 2                              -- set a max of 2 queued connections
-    mainLoop sock       
-
+chatting_main
+  = do sock <- socket AF_INET Stream 0
+       -- create socket
+       setSocketOption sock ReuseAddr 1
+       -- make socket immediately reusable - eases debugging.
+       bind sock (SockAddrInet 4242 iNADDR_ANY)
+       -- listen on TCP port 4242.
+       listen sock 2
+       -- set a max of 2 queued connections
+       mainLoop sock
 
 mainLoop :: Socket -> IO ()
-mainLoop sock = do
-    conn <- accept sock     -- accept a connection and handle it
-    runConn conn            -- run our server's logic
-    mainLoop sock           -- repeat
+mainLoop sock
+  = do conn <- accept sock
+       -- accept a connection and handle it
+       runConn conn
+       -- run our server's logic
+       mainLoop sock
+
+-- repeat
 
 runConn :: (Socket, SockAddr) -> IO ()
-runConn (sock, _) = do
-    send sock "Hello!\n"
-    close sock
+runConn (sock, _)
+  = do send sock "Hello!\n"
+       close sock
