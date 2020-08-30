6ec6b37994787ccbede11975377eb3077e298562
jongwhan <leejw51@gmail.com>
2020-08-30 18:13:59 +0800

    reformat

--- fibo/reformat.sh
+++ fibo/reformat.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-find . -name '*.hs' -type f -print0 | xargs -0 -I {} -n 1 haskell-formatter --force --input {} --output {}
+find ./src -name '*.hs' -type f -print0 | xargs -0 -I {} -n 1 haskell-formatter --force --input {} --output {}
+find ./app -name '*.hs' -type f -print0 | xargs -0 -I {} -n 1 haskell-formatter --force --input {} --output {}
