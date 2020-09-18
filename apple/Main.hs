e0342ad76b20112063fdfe8efad8d8c30085d8f3
jongwhan <leejw51@gmail.com>
2020-09-18 08:43:29 +0800

    show hex

--- apple/Main.hs
+++ apple/Main.hs
@@ -1,11 +1,13 @@
 import Data.Char
 import Data.List
 import Earth
+import qualified Sha as S
 
 main = process
 
 
 process = do
   putStrLn "hello world"
-  earth_hello
+  --earth_hello
+  S.sha_main
   
--- code/app/Examples/Hacking.hs
+++ code/app/Examples/Hacking.hs
@@ -1,6 +1,9 @@
 module Examples.Hacking where
-
-
+import qualified Data.ByteString.Char8 as BS
+import qualified Crypto.Hash.SHA256 as SHA256
+import Numeric (showHex)
+import qualified Data.ByteString
+import Numeric (showHex)
 main2=do 
     putStr "enter sha256 hash to hack="
     putStrLn ""
@@ -9,11 +12,17 @@ main2=do
 
 compute [] = putStrLn ""
 compute (x:xs)= do
-  print x
+  --let bs = SHA256.hash x
+  let s = "sha1 this string"
+  let bs= SHA256.hash $ BS.pack x
+  print bs
   compute xs
 main= do 
-  let s= 0
-  let e = 9
+  let s= '0'
+  let e = '9'
   let a=[ [x0,x1,x2] | x0 <-[s..e],x1<-[s..e], x2<-[s..e]]
   --print  a
   compute a
+
+
+
