9da809b3687f83319bdfe36e9402c8988a0f5ab0
jongwhan <leejw51@gmail.com>
2020-09-22 16:24:31 +0800

    ord

--- code/app/Examples/Recursion.hs
+++ code/app/Examples/Recursion.hs
@@ -1,15 +1,24 @@
 module Examples.Recursion where
-
+import Data.Char (ord)
 main=do
   putStrLn "Test Recursion"
-  let a= compute 3
+  let a=compute2 3 [ord '0'..ord '9']
   print a
+  print "OK"
 
-compute2 0 = [[]]
-compute2 n = [ (c:cs) | c<-x, cs<- compute2 $ n-1]
-  where x=[0..9]
+compute2 0 b = [[]]
+compute2 n b = [ (c:cs) | c<-b, cs<- (compute2  (n-1)  b  )]
 
 
-compute 0 = [""]
-compute n = [ (c:cs) | c<-x, cs<- compute $ n-1]
-  where x=['0','1','2']
+
+compute :: (Eq t, Num t) => t -> [Char] -> [[Char]]
+compute 0 b = [""]
+compute n b  = [ (c:cs) | c<-b, cs<-  (compute (n-1) b) ]
+
+--compute a  b = fmap (+a) b 
+
+compute3 0 _ = do
+  print "ZERO" 
+
+compute3 a b= do
+  print a >> print b
