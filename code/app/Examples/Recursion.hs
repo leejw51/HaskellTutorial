85b49af2411dfc6f2d024aac88f87355e8a7cb91
jongwhan <leejw51@gmail.com>
2020-09-22 12:35:31 +0800

    recursion

--- code/app/Examples/Recursion.hs
+++ code/app/Examples/Recursion.hs
@@ -2,4 +2,14 @@ module Examples.Recursion where
 
 main=do
   putStrLn "Test Recursion"
+  let a= compute 3
+  print a
 
+compute2 0 = [[]]
+compute2 n = [ (c:cs) | c<-x, cs<- compute2 $ n-1]
+  where x=[0..9]
+
+
+compute 0 = [""]
+compute n = [ (c:cs) | c<-x, cs<- compute $ n-1]
+  where x=['0','1','2']
