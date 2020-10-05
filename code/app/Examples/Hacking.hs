2599bf14a16d23f0b56b49f4e74967d6a9ea81e3
jongwhan <leejw51@gmail.com>
2020-10-05 19:22:58 +0800

    compute

--- code/app/Examples/Hacking.hs
+++ code/app/Examples/Hacking.hs
@@ -52,11 +52,21 @@ fib 0 = 1
 fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
+mycompare a myhash=
+  if ((SHA256.hash. BS2.pack) a)==myhash
+    then a
+    else ""
+
+compute [] myhash = ""
+compute (x:xs) myhash = par n1 (pseq n2 (if n1=="" then n2 else n1))
+  where n1 = mycompare x myhash
+        n2 = compute xs myhash
+
 main= do
-  let a= nfib 10
-  let b = nfib2 10
-  let c = fib 10
-  print a
-  print b 
-  print c
+  let (c,_)=  B16.decode $ BS2.pack "f66edff8c75e2a6de17e15cdd5ed5dbf11fdd1b7cb5823d573bf2aab8997a696"
+  let myhash= c
+  let a = replicateM 8 ['0'..'9']
+  --print a
+  let b=compute a myhash
+  print b
   print "OK"
