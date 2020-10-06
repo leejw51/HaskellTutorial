327d208d56c9f18ba87e5699032fccce0c6410e9
jongwhan lee <jonghwan@crypto.com>
2020-10-06 18:11:52 +0800

    OK

--- code/app/Examples/Hacking.hs
+++ code/app/Examples/Hacking.hs
@@ -9,12 +9,14 @@ import Data.Foldable
 
 mysolve:: Int -> Maybe Int
 mysolve a = do
-  if a<5 
+  if a>5 
     then return (a*100)                                                                                                                                        
     else Nothing
     
 main = do                                                                                                                               
   let grids= [1..10]
+  let b2= (runPar $ parMap mysolve grids)
+  print b2
   let b=asum (runPar $ parMap mysolve grids)
   print b
   print "OK"                                  
