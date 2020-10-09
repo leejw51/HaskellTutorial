3cd433eea22ca9bccbf4759e7355846e259d15bd
jongwhan <leejw51@gmail.com>
2020-10-09 11:05:18 +0800

    ok

--- code/app/Examples/Hacking.hs
+++ code/app/Examples/Hacking.hs
@@ -6,17 +6,28 @@ import Data.Maybe
 import Control.Monad.Par
 import Control.Monad                                                                                                                
 import Data.Foldable
+import Control.DeepSeq
+import Control.Parallel
+import Control.Applicative
 
-mysolve:: Int -> Maybe Int
-mysolve a = do
-  if a>5 
-    then return (a*100)                                                                                                                                        
+main = do
+  putStrLn "Hacking"
+  let c= replicateM 4 [0..9]
+  let b= mycompute <%> c
+  let found= foldl (<|>) empty b
+  print found
+  print "OK"
+  
+mysum xs= foldl (\acc x -> acc+x) 0 xs
+
+mycompute:: [Int] -> Maybe [Int]
+mycompute a=  do
+  let b= sum a 
+  if b==10 
+    then Just a
     else Nothing
-    
-main = do                                                                                                                               
-  let grids= [1..10]
-  let b2= (runPar $ parMap mysolve grids)
-  print b2
-  let b=asum (runPar $ parMap mysolve grids)
-  print b
-  print "OK"                                  
+
+f <%> []       = []
+f <%> (x : xs) = y `par` ys `pseq` (y : ys) where
+  y  = force $ f x
+  ys = f <%> xs
