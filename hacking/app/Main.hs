9516455ce68117c292e657e0fcd8fc083a785904
jongwhan <leejw51@gmail.com>
2020-10-11 16:35:04 +0800

    rename

--- hacking/app/Main.hs
+++ hacking/app/Main.hs
@@ -12,8 +12,7 @@ import qualified Crypto.Hash.SHA256            as S
 chars :: String
 chars = "0123456789"
 byteChars :: [C.ByteString]
-byteChars = C.pack . (: []) <$> chars
-
+byteChars =  C.pack . (: []) <$> chars
 
 bytePrefixes :: Int -> String -> [C.ByteString]
 bytePrefixes numPrefix chars = C.pack <$> replicateM numPrefix chars
@@ -23,28 +22,30 @@ myprefix=bytePrefixes 3 chars
 
 
 
-mycompute2 :: C.ByteString -> [C.ByteString]
--- prefix: to distribute sparks
-mycompute2 prefix= do
+hacking :: C.ByteString -> [C.ByteString]
+-- prefix: to distribute sparks, this is for multi threading
+-- without prefix: onely single threaded
+-- with too many prefix: then too many sparks, system down
+hacking workloads= do
   a <- byteChars
   b <- byteChars
   c <- byteChars
   d <- byteChars
   e <- byteChars
-  let src = foldl' (<>) mempty [prefix, a, b, c, d, e]
+  let src = foldl (<>) mempty [workloads, a, b, c, d, e]
   let hash= S.hash src 
   guard (target == hash)
   return src
 
 main = do
-  let found =head $foldl' (<|>) empty $   mycompute2 <%> myprefix
+  let found =head $foldl' (<|>) empty $   runsparks  hacking myprefix
   let found2= C.unpack found
   print found2
 
 
 
-(<%>) :: (NFData a, NFData b) => (a -> b) -> [a] -> [b]
-f <%> []       = []
-f <%> (x : xs) = y `par` ys `pseq` (y : ys) where
+runsparks :: NFData a1 => (a2 -> a1) -> [a2] -> [a1]
+runsparks f []       = []
+runsparks f  (x : xs) = par y (pseq ys  (y : ys)) where
   y  = force $ f x
-  ys = f <%> xs
+  ys = runsparks f  xs
